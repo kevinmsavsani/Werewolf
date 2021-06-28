5b76ad96dfff52d3323c529d269d1ff0a88cf4f1
kevinmsavsani <kevinmsavsani@gmail.com>
2021-06-28 15:03:15 +0530

    babel added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
- (function helloWorld() {
-      console.log('Hello World');
- }());
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloWorld = () => {
+    return (
+        <h1>
+            Hello World
+        </h1>
+    );
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,23 @@ module.exports = {
     mode: process.env.NODE_ENV || "development",
     resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
     devServer: { contentBase: path.join(__dirname, "src") },
+    module: {
+        rules: [
+            { 
+                test: /\.(js|jsx)$/, 
+                exclude: /node_modules/, 
+                use: ["babel-loader"] 
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader"],
+            },
+            { 
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ["file-loader"] 
+            },
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, "src", "index.html"),
